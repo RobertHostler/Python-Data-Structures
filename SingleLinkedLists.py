5cf6007b73fb88013edc1eae12a6bccf2b459d53
Robert <robert.hostler@protonmail.com>
2019-11-05 20:19:15 -0500

    added retrieve function to singly linked list; Some syntax was simplified

--- SingleLinkedLists.py
+++ SingleLinkedLists.py
@@ -20,19 +20,29 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def len(self):
+        if self.head == None:
+            return 0
+        else:
+            length = 1
+            current_node = self.head
+            while current_node.next():
+                length += 1
+                current_node = current_node.next()
+            return length
+
     def add(self, value):
         if self.head == None:
             self.head = Node(value)
         else:
             current_node = self.head
-            while current_node.next() != None:
+            while current_node.next():
                 current_node = current_node.next()
             current_node.link(Node(value))
 
     def insert(self, index, value):
         if self.head == None:
             self.add(value)
-
         if index < 0:
             print("Index out of bounds")
             return None
@@ -44,13 +54,11 @@ class LinkedList:
         else:
             current_node = self.head
             inserted_node = Node(value)
-
             for _ in range(index - 1):
                 if current_node.next() == None:
                     print("Index out of bounds")
                     return None
                 current_node = current_node.next()
-
             next_node = current_node.next()
             current_node.link(inserted_node)
             inserted_node.link(next_node)
@@ -70,19 +78,30 @@ class LinkedList:
                 current_node = current_node.next()
             previous_node.link(current_node.next())
 
+    def retrieve(self, index):
+        if self.head == None:
+            return None
+        else:
+            current_node = self.head
+            for _ in range(index):
+                if current_node.next() == None:
+                    print("Index out of bounds")
+                    return None
+                current_node = current_node.next()
+            return current_node.get_value()
+
     def find(self, value):
         current_node = self.head
         i = 0
-        while current_node != None:
+        while current_node:
+            if current_node.next() == None:
+                print("The requested element was not found in this linked list")
+                return None
             if current_node.get_value() == value:
-                break
+                return i
             else:
                 current_node = current_node.next()
                 i += 1
-        if current_node == None:
-            return - 1
-        else:
-            return i
 
     def __str__(self):
         if self.head == None:
@@ -90,7 +109,7 @@ class LinkedList:
         else:
             linked_string = "| "
             current_node = self.head
-            while current_node != None:
+            while current_node:
                 linked_string += str(current_node.get_value()) + " -> "
                 current_node = current_node.next()
             linked_string += str(None) + " |"
